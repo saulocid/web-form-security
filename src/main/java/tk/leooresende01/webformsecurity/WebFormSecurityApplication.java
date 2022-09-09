5f4458eb92d5c70352168fe7107b60179bf54cf5
leooresende01 <leosousa1211@gmail.com>
2022-09-08 22:51:36 -0300

    Startando e ignorando arquivos do maven

---
+++ src/main/java/tk/leooresende01/webformsecurity/WebFormSecurityApplication.java
@@ -0,0 +1,13 @@
+package tk.leooresende01.webformsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFormSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFormSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tk/leooresende01/webformsecurity/WebFormSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.leooresende01.webformsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFormSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
